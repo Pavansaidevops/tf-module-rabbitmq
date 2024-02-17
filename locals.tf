4943102e24cd774e31769c1412619a76bb6a8683
Pavan Sai <pavansaidevops7@gmail.com>
2024-02-17 20:38:56 +0530

    terraform rabbitmq

---
+++ locals.tf
@@ -0,0 +1,4 @@
+locals {
+  name_prefix = "${var.env}-rabbitmq"
+  tags        = merge(local.tags, { tf-module-name = "rabbitmq" }, { env = var.env }
+}
---
+++ userdata.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+yum install ansible -y &>>/userdata.log
+ansible-pull -i localhost, -U https://github.com/Pavansaidevops/roboshop-ansible.git main.yml -e component = rabbitmq
