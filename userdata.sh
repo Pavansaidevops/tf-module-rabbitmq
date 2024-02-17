4b0fdce1accea780d24d426919128be8ab055de4
Pavan Sai <pavansaidevops7@gmail.com>
2024-02-17 20:39:38 +0530

    terraform rabbitmq

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 yum install ansible -y &>>/userdata.log
-ansible-pull -i localhost, -U https://github.com/Pavansaidevops/roboshop-ansible.git main.yml -e component = rabbitmq
+ansible-pull -i localhost, -U https://github.com/Pavansaidevops/roboshop-ansible.git main.yml -e component = rabbitmq &>>/userdata.log
+
